b3397c15b92e023f87fc75a46eb4c178a58909b3
wexist2003 <neoscerberis@gmail.com>
2023-10-11 21:34:45 +0300

    modul4

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-print("Hello World!!")
-print("Hello World!!")
+def split_list(grade):
+    list_lower = []
+    list_higher = []
+    if not grade == []:
+        sum = 0
+        for el in grade:
+            sum += el
+        avg = sum / len(grade)
+        for el in grade:
+            if el <= avg:
+                list_lower.append(el)
+            else:
+                list_higher.append(el)
+    kortlist = (list_lower, list_higher)
+    return kortlist
+
+
+a = []
+print(split_list(a))
