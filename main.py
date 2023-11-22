82f299d41f2f63fc76851142d3e89488541c762e
wexist2003 <neoscerberis@gmail.com>
2023-11-22 21:36:42 +0200

    m9m1

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+def input_error(function):
+    def inner(*args, **kwargs):
+        try:
+            return function(*args, **kwargs)
+        except KeyError:
+            return "Wrong name"
+        except ValueError as exception:
+            return exception.args[0]
+        except IndexError:
+            return "Give me name and phone please"
+        except TypeError:
+            return "Wrong command"
+
+    return inner
+
+
+@input_error
 def handler(command_list):
     len_command = len(command_list)
     match len_command:
@@ -14,12 +31,14 @@ def handler(command_list):
             try:
                 return func_add(user_name, user_phone)
             except UnboundLocalError:
-                return "Give me name and phone please"
+                raise ValueError("Give me name and phone please")
+                # return "Give me name and phone please"
         case "change":
             try:
                 return func_change(user_name, user_phone)
             except UnboundLocalError:
-                return "Give me name and phone please"
+                raise ValueError("Give me name and phone please")
+                # return "Give me name and phone please"
         case "exit":
             return func_exit()
         case "good bye":
@@ -32,29 +51,35 @@ def handler(command_list):
             try:
                 return func_phone(user_name)
             except UnboundLocalError:
-                return "Enter user name"
+                raise ValueError("Enter user name")
+                # return "Enter user name"
         case "show all":
             return func_show_all()
         case _:
-            return "Enter correct command"
+            raise ValueError("Enter correct command")
+            # return "Enter correct command"
     return
 
 
+@input_error
 def func_add(user_name, user_phone):
     new_contact = {user_name: user_phone}
     phone_book.update(new_contact)
     return "Added"
 
 
+@input_error
 def func_change(user_name, user_phone):
     phone_book[user_name] = user_phone
     return "Changed"
 
 
+@input_error
 def func_exit():
     return "Good bye!"
 
 
+@input_error
 def func_hello():
     return "How can I help you?"
 
@@ -83,16 +108,19 @@ def main():
                         print(result)
 
 
+@input_error
 def func_phone(user_name):
     return phone_book[user_name]
 
 
+@input_error
 def func_show_all():
     result = ""
     for name, phone in phone_book.items():
-        result = result + f"{name}: {phone}, "
-    return result.removesuffix(", ")
+        result = result + f"{name} : {phone} \n"
+    return result.removesuffix(" \n")
 
 
-phone_book = {}
-main()
+if __name__ == "__main__":
+    phone_book = {}
+    main()
